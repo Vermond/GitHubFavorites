529d3a2f5829bd6e993d00946a21178110aab32a
구진수 <jsgu@macrogen.com>
2025-01-22 10:40:41 +0900

    - Add project templates

---
+++ GitHubFavorites/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFavorites
+//
+//  Created by Jinsu Gu on 1/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubFavoritesTests/GitHubFavoritesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFavoritesTests.swift
+//  GitHubFavoritesTests
+//
+//  Created by Jinsu Gu on 1/22/25.
+//
+
+import Testing
+@testable import GitHubFavorites
+
+struct GitHubFavoritesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
